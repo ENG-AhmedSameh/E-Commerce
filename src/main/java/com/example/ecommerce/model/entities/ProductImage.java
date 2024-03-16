3ca7278c1e1231f5ebe00835f1a5e9b2f98a5199
Ahmed Sameh <eng.ahmedsameh99@gmail.com>
2024-03-16 15:10:10 +0200

    merged all branches

--- src/main/java/com/example/ecommerce/model/entities/ProductImage.java
+++ src/main/java/com/example/ecommerce/model/entities/ProductImage.java
@@ -13,6 +13,7 @@ public class ProductImage {
     @EmbeddedId
     private ProductImageId id;
 
+    //
     @MapsId("productId")
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "product_id", nullable = false)
